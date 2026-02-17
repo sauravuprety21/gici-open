faf7571759fab79dd4fe42569e7800a5c15a6c7b
saurav <saurav.uprety1@ucalgary.ca>
2026-02-17 12:19:05 -0700

    Touch up

--- src/fusion/rtk_imu_camera_rrr_estimator.cpp
+++ src/fusion/rtk_imu_camera_rrr_estimator.cpp
@@ -399,28 +399,28 @@ bool RtkImuCameraRrrEstimator::estimate()
       }
     }
 
-  // Check if we continuously cannot fix ambiguity, while we have good observations
-  if (rtk_options_.use_ambiguity_resolution) {
-    const double thr = gnss_base_options_.good_observation_max_reject_ratio;
-    if (isGnssGoodObservation() && ratio_pseudorange < thr && 
-        ratio_phaserange < thr && ratio_doppler < thr) {
-      if (curState().status != GnssSolutionStatus::Fixed){
-        if(has_first_ambguity_fix_) num_continuous_unfix_++;
-      } 
-      else {
+    // Check if we continuously cannot fix ambiguity, while we have good observations
+    if (rtk_options_.use_ambiguity_resolution) {
+      const double thr = gnss_base_options_.good_observation_max_reject_ratio;
+      if (isGnssGoodObservation() && ratio_pseudorange < thr && 
+          ratio_phaserange < thr && ratio_doppler < thr) {
+        if (curState().status != GnssSolutionStatus::Fixed){
+          if(has_first_ambguity_fix_) num_continuous_unfix_++;
+        } 
+        else {
+          num_continuous_unfix_ = 0;
+          has_first_ambguity_fix_ = true; 
+        }
+      }
+      else num_continuous_unfix_ = 0;
+      if (num_continuous_unfix_ > 
+          gnss_base_options_.reset_ambiguity_min_num_continuous_unfix && has_first_ambguity_fix_) {
+        LOG(INFO) << "Continuously unfix under good observations. Clear current ambiguities.";
+        resetAmbiguityEstimation();
         num_continuous_unfix_ = 0;
-        has_first_ambguity_fix_ = true; 
       }
     }
-    else num_continuous_unfix_ = 0;
-    if (num_continuous_unfix_ > 
-        gnss_base_options_.reset_ambiguity_min_num_continuous_unfix && has_first_ambguity_fix_) {
-      LOG(INFO) << "Continuously unfix under good observations. Clear current ambiguities.";
-      resetAmbiguityEstimation();
-      num_continuous_unfix_ = 0;
-    }
   }
-
   // Image processes
   if (new_state_type == IdType::cPose) {
     // update landmarks to frontend
